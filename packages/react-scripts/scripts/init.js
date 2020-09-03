72efd4c190d26ab8ed82066f00f4e19b07548c84
hupengpeng <hupp@xiaoma.wang>
2020-09-03 18:27:17 +0800

    init change

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -3,6 +3,31 @@ process.on('unhandledRejection', err => {
   });
 
 
-module.exports = function() {
-  console.log(11111111)
+const fs = require('fs-extra')
+const { defaultBrowsers } = require('react-dev-utils/browsersHelper');
+const paths = require('../config/paths');
+const ownPath = paths.ownPath;
+const appPath = paths.appPath;
+const path = require('path')
+
+module.exports = function(
+  appPath,
+  appName,
+  verbose,
+  originalDirectory,
+  templateName
+) {
+  const ownPackage = require(path.join(ownPath, 'package.json'));
+  const appPackage = {
+    eslintConfig: 'react-app',
+    browserslist: defaultBrowsers,
+    dependencies: ownPackage.dependencies
+  }
+  fs.writeFileSync(
+    path.join(appPath, 'package.json'),
+    JSON.stringify(appPackage, null, 2) + os.EOL
+  );
+  fs.copy(`${ownPath}/config`, `${appPath}/config`)
+  .then(() => console.log('create config completed'))
+  .catch((e) => console.log(error))
 }
