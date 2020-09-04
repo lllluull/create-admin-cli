2250066dd0ec57da40c89cb123b7bda544a92d65
hupengpeng <hupp@xiaoma.wang>
2020-09-04 14:56:44 +0800

    eslint perrtir

---
+++ packages/react-scripts/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+    },
+    extends: ["prettier", "prettier/standard", "prettier/react", "plugin:react/recommended"],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error",
+    },
+    parser: "babel-eslint",
+    settings: {
+      react: {
+        pragma: "React",
+        version: "detect",
+      },
+    },
+  }
