0056148edbfd7449baa8929d7c10f598deb860b6
hupengpeng <hupp@xiaoma.wang>
2020-09-04 11:32:33 +0800

    log

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -394,26 +394,26 @@ module.exports = function(webpackEnv) {
               test: /\.(js|mjs)$/,
               exclude: /@babel(?:\/|\\{1,2})runtime/,
               loader: require.resolve('babel-loader'),
-              options: {
-                babelrc: false,
-                configFile: false,
-                compact: false,
-                presets: [
-                  [
-                    require.resolve('babel-preset-react-app/dependencies'),
-                    { helpers: true },
-                  ],
-                ],
-                cacheDirectory: true,
-                // See #6846 for context on why cacheCompression is disabled
-                cacheCompression: false,
+              // options: {
+              //   babelrc: false,
+              //   configFile: false,
+              //   compact: false,
+              //   presets: [
+              //     [
+              //       require.resolve('babel-preset-react-app/dependencies'),
+              //       { helpers: true },
+              //     ],
+              //   ],
+              //   cacheDirectory: true,
+              //   // See #6846 for context on why cacheCompression is disabled
+              //   cacheCompression: false,
                 
-                // Babel sourcemaps are needed for debugging into node_modules
-                // code.  Without the options below, debuggers like VSCode
-                // show incorrect code and set breakpoints on the wrong lines.
-                sourceMaps: shouldUseSourceMap,
-                inputSourceMap: shouldUseSourceMap,
-              },
+              //   // Babel sourcemaps are needed for debugging into node_modules
+              //   // code.  Without the options below, debuggers like VSCode
+              //   // show incorrect code and set breakpoints on the wrong lines.
+              //   sourceMaps: shouldUseSourceMap,
+              //   inputSourceMap: shouldUseSourceMap,
+              // },
             },
             // "postcss" loader applies autoprefixer to our CSS.
             // "css" loader resolves paths in CSS and adds assets as dependencies.
